12c67784a56972095163489858651917091a7018
Guilherme Rocha <guirocha12395@gmail.com>
2022-01-11 00:54:33 -0300

    Atualizando modal e colocando estrutura de configuração

--- js/index.js
+++ js/index.js
@@ -16,24 +16,67 @@ var vencedor = '_'
 var Empate = 0
 var play1 = 0, play2 = 0, draw  = 0;
 
+var root = document.querySelector(':root');
+var colorX = document.querySelector('#colorX');
+var colorO = document.querySelector('#colorO');
+
+colorX.value = getComputedStyle(root).getPropertyValue('--colorX')
+colorO.value = getComputedStyle(root).getPropertyValue('--colorO')
+
+colorX.addEventListener('change', (event) => refreshColor(event.target.name,event.target.value));
+colorO.addEventListener('change', (event) => refreshColor(event.target.name,event.target.value));
+
+var refreshColor = (propertyColor,cor) =>{
+    root.style.setProperty(`${propertyColor}`, `${cor}`)
+}
+
+function openNav() {
+    document.querySelector('aside').style.display = 'flex';
+}
+
+
+document.querySelector('.fas.fa-cog').addEventListener('click', (event) => document.querySelector('aside').style.cssText = `display: flex`)
+document.querySelector('.far.fa-times-circle').addEventListener('click', (event) => document.querySelector('aside').style.cssText = `display: none`);
+
 for(var i=0; i<9; i++) {
 	item[i].addEventListener('click', (event) => {
 		if( !(event.target.classList.contains("ativo")) && (vencedor=='_')) {
-			event.target.querySelector('.fas').classList.add(`${jogador}`); //preenche a casa com X ou O
+			event.target.querySelector('.fas').classList.add(`${jogador}`); 
             event.target.classList.add("ativo");
 
             vencedor = vitoria(jogador);
             if(vencedor == 'fa-times'){
                 textPlay1.textContent = ++play1
                 finallyBox.style.display = 'flex'
+                finallyBox.querySelector('.champion').classList.remove("fadotcircle");
+                finallyBox.querySelector('.champion').classList.remove("empate");
+                finallyBox.querySelector('.champion').classList.add(`${jogador.replaceAll('-','')}`)
+                finallyBox.querySelector('.fas').classList.remove("fa-dot-circle");
+                finallyBox.querySelector('.fas').classList.remove("empate");
+                finallyBox.querySelector('.fas').classList.add(`${jogador}`)
+                finallyBox.querySelector('a').textContent = 'Ganhou'
             }else if(vencedor == "fa-dot-circle"){
                 textPlay2.textContent = ++play2
                 finallyBox.style.display = 'flex'
+                finallyBox.querySelector('.champion').classList.remove("fatimes");
+                finallyBox.querySelector('.champion').classList.remove("empate");
+                finallyBox.querySelector('.champion').classList.add(`${jogador.replaceAll('-','')}`)
+                finallyBox.querySelector('.fas').classList.remove("fa-times");
+                finallyBox.querySelector('.fas').classList.remove("empate");
+                finallyBox.querySelector('.fas').classList.add(`${jogador}`)
+                finallyBox.querySelector('a').textContent = 'Ganhou'
             }
             Empate = empate();
             if(vencedor=='_' && Empate > 8){
                 textDraw.textContent = ++draw
                 finallyBox.style.display = 'flex'
+                finallyBox.querySelector('.champion').classList.remove("fatimes");
+                finallyBox.querySelector('.champion').classList.remove("fadotcircle");
+                finallyBox.querySelector('.champion').classList.add(`empate`)
+                finallyBox.querySelector('.fas').classList.remove("fa-times");
+                finallyBox.querySelector('.fas').classList.remove("fa-dot-circle");
+                finallyBox.querySelector('.fas').classList.add(`empate`)
+                finallyBox.querySelector('a').textContent = 'Empate'
             }
 
 			trocarJogador(); 
@@ -133,3 +176,5 @@ var empate = () => {
     }
     return cont;
 }
+
+var hexToRgb = hex => hex.match(/\w\w/g).map(x=>+`0x${x}`)
